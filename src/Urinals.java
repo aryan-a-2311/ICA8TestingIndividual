7b89793988fc59b949864227a3e15cb170995cfe
aryan-a-2311 <aagar125@asu.edu>
2022-10-27 17:59:38 -0700

    Commit with full keyboard input functionality

--- src/Urinals.java
+++ src/Urinals.java
@@ -1,22 +1,37 @@
+import java.util.Scanner;
+
 public class Urinals {
-    public boolean validateInputLength(String bitString)
+    static String stringToCheck;
+
+    public static void keyboardInput()
     {
-        return bitString.length() <= 20;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        stringToCheck = sc.next();
     }
 
-    public boolean validateInputType(String bitString)
+    public static int validateInputLength(String bitString)
+    {
+        if(bitString.length() > 20)
+        {
+            return -1;
+        }
+        return 1;
+    }
+
+    public static int validateInputType(String bitString)
     {
         for(int i=0; i < bitString.length(); i++)
         {
             if(bitString.charAt(i) != '0' && bitString.charAt(i) != '1')
             {
-                return false;
+                return -1;
             }
         }
-        return true;
+        return 1;
     }
 
-    public int findAvailability(String bitString) {
+    public static int findAvailability(String bitString) {
         if(bitString.length() == 1){
             if(bitString.charAt(0) == '0')
                 return 1;
@@ -65,5 +80,27 @@ public class Urinals {
             }
         }
         return answer;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================== MENU ==================");
+        System.out.println("1. Input from Keyboard");
+        System.out.println("2. Inputs from File");
+        System.out.println("Select your Choice(1 or 2): ");
+        int choice = sc.nextInt();
+
+        if(choice == 1)
+        {
+            keyboardInput();
+            if(validateInputLength(stringToCheck) != -1 && validateInputType(stringToCheck) != -1)
+            {
+                System.out.println("Free Urinals: " + findAvailability(stringToCheck));
+            }
+            else{
+                System.out.println("Free Urinals: -1");
+            }
+        }
     }
 }
--- test/UrinalsTest.java
+++ test/UrinalsTest.java
@@ -3,31 +3,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UrinalsTest {
-    Urinals urinal1 = new Urinals();
 
     @Test
     void testvalidateInputLength() {
         System.out.println("======ARYAN AGARWAL====== TEST ONE EXECUTED (Length < 20)");
 
-        assertTrue(urinal1.validateInputLength("1010101010"));
+        assertEquals(1, Urinals.validateInputLength("1010101010"));
     }
 
     @Test
     void testvalidateInputType() {
         System.out.println("======ARYAN AGARWAL====== TEST TWO EXECUTED (Only 0's and 1's)");
 
-        assertTrue(urinal1.validateInputType("10101010"));
+        assertEquals(1, Urinals.validateInputType("10101010"));
     }
 
     @Test
     void testfindAvailability() {
         System.out.println("======ARYAN AGARWAL====== TEST THREE EXECUTED (One bit String)");
-        assertEquals(1, urinal1.findAvailability("0"));
+        assertEquals(1, Urinals.findAvailability("0"));
 
         System.out.println("======ARYAN AGARWAL====== TEST FOUR EXECUTED (String containing two consecutive 1's)");
-        assertEquals(-1, urinal1.findAvailability("01011100010"));
+        assertEquals(-1, Urinals.findAvailability("01011100010"));
 
         System.out.println("======ARYAN AGARWAL====== TEST FIVE EXECUTED (A valid string that should return the maximum number of more stalls that can be occupied )");
-        assertEquals(3, urinal1.findAvailability("00100010100010"));
+        assertEquals(3, Urinals.findAvailability("00100010100010"));
     }
 }
